c73b93f9127109ad4b7cfcedb2e1e0755c87e284
likhithabc <119933056+likhithabc@users.noreply.github.com>
2023-03-01 16:04:30 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTML Forms </title>
+    </head>
+    <body>
+        <form>
+        <br>Enter your first name:<br>
+        <input type="text" name="name" required >
+        <br>Enter your second name:<br>
+        <input type="text" name="name">
+      
+        <input type="submit" name="submit"> 
+        </form>
+    </body>
+    </html>
